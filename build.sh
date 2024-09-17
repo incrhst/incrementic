a065b6a74b079864d69952fcaf32b7ac38a83ffd
incrhst <110049091+incrhst@users.noreply.github.com>
2024-09-17 13:46:36 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,2 @@
-sed -i s/document\.createElement\(\"udesly-banner\"\)// theme/*.html
-sed -i s/document\.createElement\(\"udesly-banner\"\)// theme/*/*.html
-#find theme -type f -name "*.html" -exec sh -c '
-#    for file do
-#        tmpfile=$(mktemp)
-#        sed "$ d" "$file" > "$tmpfile" && echo "</body>" >> "$tmpfile" && mv "$tmpfile" "$file"
-#    done
-#' sh {} +
-npm run build
+echo "build actually does nothing anymore"
 cp robots-production.txt public/robots.txt
